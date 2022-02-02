1f739949381b23e0bc78884aa9bfe3657d07430f
Beau Nouvelle <beau@getswifty.dev>
2022-02-02 12:01:14 +1100

    Initial Commit

---
+++ Shared/PocketCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketCloudApp.swift
+//  Shared
+//
+//  Created by Beau Nouvelle on 2/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct PocketCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
