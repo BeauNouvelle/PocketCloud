5da02b6b6df75e6fd7975744e940855c910e8ccf
Beau Nouvelle <beau@getswifty.dev>
2022-02-10 14:07:32 +1100

    version 1 complete

---
+++ server/Notifications.swift
@@ -0,0 +1,12 @@
+//
+//  Notifications.swift
+//  PocketCloud (iOS)
+//
+//  Created by Beau Nouvelle on 10/2/2022.
+//
+
+import Foundation
+
+extension Notification.Name {
+  static let serverFilesChanged = Notification.Name("serverFilesChanged")
+}
